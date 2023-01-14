ff376550e79dd1af5ccd45bb1a1e276c29335b50
SahanPerera96 <dsahanperera@gmail.com>
2023-01-14 23:41:24 +0530

    Add Button movement

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+
+
+function changePosition(e, color) {
+  let left = parseFloat(e.target.style.left);
+  let top = parseFloat(e.target.style.top);
+  let randomLeft = getChangableRandom(left);
+  let randomTop = getChangableRandom(top);
+  e.target.style.left = `${randomLeft}%`;
+  e.target.style.top = `${randomTop}%`;
+}
+const getRandom = () => {
+  return Math.ceil(Math.random() * 100)
+}
+const getChangableRandom = (checkValue) => {
+  let random = getRandom();
+  // console.log("checkValue", checkValue, random);
+  // while (!(random < checkValue + coefficents && random > checkValue - coefficents)) {
+  //   random = getRandom()
+  // }
+  return random;
+}
 
 export default function Home() {
   return (
@@ -17,27 +35,50 @@ export default function Home() {
       <main className={styles.main}>
 
 
+        <p>Would you like to go on a date with me?</p>
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+          <div className={styles.center}>
+            <button id="noButton"
+              className={styles.button_negative}
+              onMouseEnter={(e) => changeBackground(e, "red")}
+              onMouseLeave={(e) => changeBackground(e, "dodgerblue")}
+            >No</button>
+
+          </div>
+          <div className={styles.center}>
+
+            <button id="yesButton"
+              className={styles.button_negative}
+            // onMouseEnter={(e) => changePosition(e)}
+            // onMouseLeave={(e) => changePosition(e)}
+            >Yes</button>
           </div>
         </div>
+        <div className={styles.fullView}>
+          <div
+            className={styles.button_wrapper}
+            style={{ top: '1px', left: '1px' }}
+            onMouseEnter={(e) => changePosition(e)}
+            onClick={(e) => changePosition(e)}
+          >
+            <button id="noButton"
+              className={styles.button_negative}
+              onClick={() => {
+                alert('Like you are my Type')
+              }}
+            >No</button>
+          </div>
 
+          <button id="yesButton"
 
+            className={styles.button_positive}
+            style={{ top: '1px', right: '1px' }}
+            onMouseEnter={(e) => changePosition(e)}
+            onClick={() => {
+              alert('See you tonight')
+            }}
+          >Yes</button>
+        </div>
       </main>
     </>
   )
